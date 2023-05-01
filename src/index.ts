52e5db4380d2ded057aebd784b4aff6331798a9e
sharifmrahat <sharifmrahat@gmail.com>
2023-05-01 20:29:06 +0600

    Primitive types in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 const course: string = 'apollo next level web development mission'
 
 console.log(course)
+
+//Primitive type
+//string
+//number
+//boolean
+//undefined
+//null
+
+const number = 313
