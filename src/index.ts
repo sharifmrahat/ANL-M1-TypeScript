0a10fd098e1d203675dc31751c15f614a34d69b8
sharifmrahat <sharifmrahat@gmail.com>
2023-05-01 21:02:24 +0600

    Object, Literal Types and Optional types

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,30 @@ const rolls: number[] = [34, 12, 23, 3, 4];
 const myArray: [number, string] = [27, "Sharif"];
 
 // myArray[0] = 'abcd' //string is not assignable to number
+
+//Object Type
+
+const user: {
+  name: string;
+  age: number;
+  isMarried: boolean;
+  skills: string[];
+  phone?: number; // optional type
+} = {
+  name: "Arman",
+  age: 28,
+  isMarried: false,
+  skills: ["js", "ts", "react"],
+};
+
+
+// Literal type
+const company: {
+    name: 'The Consolers',
+    employee: number,
+    location: string,
+} = {
+    name: 'The Consolers', // literal type er value chara onno kichu dile error dibe
+    employee: 2203,
+    location: 'Bangladesh'
+}
