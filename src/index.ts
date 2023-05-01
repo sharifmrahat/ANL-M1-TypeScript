5b5bccacbd06e0422c587b0b61c1750a5cb7d52a
sharifmrahat <sharifmrahat@gmail.com>
2023-05-01 20:46:02 +0600

    array and tuples type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const course: string = 'apollo next level web development mission'
+const course: string = "apollo next level web development mission";
 
-console.log(course)
+console.log(course);
 
 //Primitive type
 //string
@@ -9,4 +9,11 @@ console.log(course)
 //undefined
 //null
 
-const number = 313
+const number = 313;
+
+const fruits: string[] = ["apple", "orange"];
+const rolls: number[] = [34, 12, 23, 3, 4];
+
+const myArray: [number, string] = [27, "Sharif"];
+
+// myArray[0] = 'abcd' //string is not assignable to number
