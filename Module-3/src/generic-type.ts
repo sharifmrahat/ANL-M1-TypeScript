147ae27d689572b23f60de06e459a5af9903e7af
sharifmrahat <sharifmrahat@gmail.com>
2023-05-03 22:51:56 +0600

    generic type is added

---
+++ Module-3/src/generic-type.ts
@@ -0,0 +1,21 @@
+//Generic type
+
+type GenericArray<T> = Array<T>;
+
+const myNumbersArray: GenericArray<number> = [1, 23, 55];
+const myStringsArray: GenericArray<string> = ["ab", "cd", "ef"];
+
+const myStudentsArray: GenericArray<{ name: string; roll: number }> = [
+  { name: "xyz", roll: 5 },
+  { name: "abc", roll: 99 },
+];
+
+//Note: We can set Multiple argument as generic type like: GenericArray<X, Y, Z>
+
+type GenericTuple<X, Y, Z> = [X, Y, Z]; //created a generic tuple type
+
+const genericTupleArray: GenericTuple<string, number, { company: string }> = [
+  "Name",
+  123,
+  { company: "nothing" },
+];
